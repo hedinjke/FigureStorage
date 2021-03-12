32fb15e73d715b1125d78772ae9c7c08a1a4710b
Alexey Fominykh <43303353+hedinjke@users.noreply.github.com>
2021-03-12 15:32:20 +0300

    SQUASHED fifth commit quaaad!

---
+++ FigureStorage.DTO/NewFolder/OneMoreNewClass.cs
@@ -0,0 +1,13 @@
+﻿namespace FigureStorage.DTO
+{
+	public class OneMoreNewClass
+	{
+		public OneMoreNewClass()
+		{
+			// some .ctor
+		}
+        
+		public void SomeMethod() {}
+		public void SomeNewFunctionality() {}
+	}
+}
